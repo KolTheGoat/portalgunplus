e5e15a9c106eb8d967cbda4fd0adae6de2159c3b
KolTheGoat <bearkol@gmail.com>
2026-06-12 15:53:12 +0300

    Initial commit

---
+++ src/client/java/me/portalgun/portalgunplus/client/FirstClient.java
@@ -0,0 +1,10 @@
+package me.portalgun.portalgunplus.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/me/portalgun/portalgunplus/client/FirstDataGenerator.java
@@ -0,0 +1,11 @@
+package me.portalgun.portalgunplus.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FirstDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
